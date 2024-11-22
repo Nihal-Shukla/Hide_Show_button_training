26f223349ea1d481542ef162fdbf3c3191666fd1
Nihal Shukla <nshukla@supplyvalid.com>
2024-11-22 07:37:12 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [status,setStatus]=React.useState(true)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className='button'>
+    {
+    status ? <h1> Hello World !</h1> : null
+}
+
+<button onClick={()=>setStatus(false)}>Hide</button><br></br>
+  <button onClick={()=>setStatus(true)}>Show</button>
+  </div>
+    </>
   );
 }
 
